8768edc761703b67810f34fab50b29ab06ba380c
zeynepsteel <lilyfoster1987@gmail.com>
2021-08-13 20:30:27 -0400

    second meassage for arrays

--- src/com/syntax/git/arrayss.java
+++ src/com/syntax/git/arrayss.java
@@ -4,7 +4,9 @@ public class arrayss {
 
 	public static void main(String[] args) {
 		int[] arr= {7,5,8,4,5,6,8};
-		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 
 }
