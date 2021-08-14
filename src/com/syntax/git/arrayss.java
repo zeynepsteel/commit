2646f653f37c63284deefc8481f16770ac53c206
zeynepsteel <lilyfoster1987@gmail.com>
2021-08-13 20:31:53 -0400

    third message for arrays

--- src/com/syntax/git/arrayss.java
+++ src/com/syntax/git/arrayss.java
@@ -5,8 +5,9 @@ public class arrayss {
 	public static void main(String[] args) {
 		int[] arr= {7,5,8,4,5,6,8};
 		for (int i = 0; i < arr.length; i++) {
+			if(arr[i]%2==0) {
 			System.out.println(arr[i]);
-		}
+		}}
 	}
 
 }
